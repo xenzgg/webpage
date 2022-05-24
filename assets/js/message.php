1d318820d26d50f35eb93be93dbe90cc2a0dc7b2
itshypax <me@hypax.wtf>
2022-05-24 12:10:47 +0200

    Update message.php

--- assets/js/message.php
+++ assets/js/message.php
@@ -2,14 +2,14 @@
 <?php
   $name = htmlspecialchars($_POST['vorname']);
   $email = htmlspecialchars($_POST['email']);
-  $phone = htmlspecialchars($_POST['playername']);
-  $website = htmlspecialchars($_POST['age']);
+  $playername = htmlspecialchars($_POST['playername']);
+  $age = htmlspecialchars($_POST['age']);
   $message = htmlspecialchars($_POST['freitext']);
-  if(!empty($email) && !empty($age) && !empty($email) && !empty($playername)){
+  if(!empty($name) && !empty($age) && !empty($email) && !empty($playername)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "kontakt@xenz.gg"; //enter that email address where you want to receive all messages
+      $receiver = "kontakt@xenz.gg";
       $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
+      $body = "Name: $name\nE-Mail: $email\Gamertag: $playername\nAlter: $age\n\nnachricht:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
          echo "Your message has been sent";
